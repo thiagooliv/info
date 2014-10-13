91dda63941022be20aad55ca76bf448e581817c6
E-Create <E-Create@E-Create-PC>
2014-10-13 16:57:51 -0300

    Modificação no arquivo

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo "It's alive <br><br>";
+        echo "It's alive!! <br><br>";
         phpinfo()
         ?>
     </body>
