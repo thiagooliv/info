6b8f8f12a18dfbc7ba3f0d334d9d743badde9a21
E-Create <E-Create@E-Create-PC>
2014-10-13 17:08:46 -0300

    Correção de codificação

--- lib/config.php
+++ lib/config.php
@@ -1,5 +1,18 @@
-<?php
-    $msg = array("status"=>"Info","mensagem"=>"Nos desculpamos pela inconveniência.");
-    print_r($msg)
-?>
-
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            $msg = array("status"=>"Info","mensagem"=>"Nos desculpamos pela inconveniência.");
+            print_r($msg)
+        ?>
+    </body>
+</html>
